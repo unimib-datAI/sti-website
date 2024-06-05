280e16f7414f2b1ec7f11b7c8159bc029ff71278
Elia Guarnieri <elia.guarnieri@gmail.com>
2024-06-05 16:26:22 +0200

    chore: Update base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://unimibinside.github.io/MammoTab-Website/",
+  base: "/MammoTab-Website/",
   integrations: [tailwind()],
 });
