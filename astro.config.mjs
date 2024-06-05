7015ab46d812592410608773b3606cea7c89171c
Elia Guarnieri <elia.guarnieri@gmail.com>
2024-06-05 17:08:09 +0200

    chore: Update base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+const isProd = import.meta.env.PROD;
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://unimibinside.github.io/MammoTab-Website/",
-  base: "/MammoTab-Website/",
+  base: isProd ? "/MammoTab-Website/" : "/",
   integrations: [tailwind()],
 });
