295b979283377a3d0043be640eae76b135b0741e
Elia Guarnieri <elia.guarnieri@gmail.com>
2024-07-15 22:41:29 +0200

    fix header accent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-const isProd = import.meta.env.PROD;
+// const isProd = import.meta.env.PROD;
+const isProd = true;
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://unimibinside.github.io/sti-website/",
-  base: isProd ? "/sti-website" : "/",
+  base: "/sti-website",
+  trailingSlash: "never",
   integrations: [tailwind(), react()],
 });
